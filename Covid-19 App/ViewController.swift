36236704cee6926946106933449a8fc51e97884a
Mark Lawrence <mpmjlaw@icloud.com>
2020-04-18 22:20:33 -0500

    Initial Commit

---
+++ Covid-19 App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Covid-19 App
+//
+//  Created by Mark Lawrence on 4/18/20.
+//  Copyright © 2020 Mark Lawrence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
